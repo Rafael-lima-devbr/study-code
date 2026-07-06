77adc22ca39e32e5d1648f5eeac1b1ab5f6f24c3
Rafael Lima <rafaelima1250@gmail.com>
2026-07-05 23:59:57 -0300

    Add Tetris game logic in tetris.cpp
    Implement Tetris game logic to calculate points based on block stacking.

---
+++ cpp/exercises/UFBA_GruPro_Intermediary/tetris.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+int M, N, pontos = 0;
+
+cin >> N >> M;
+stack <int> blocos;
+
+for (int i = 0; i <N; i++) {
+    int atual, resultado;
+    cin >> atual;
+    if (blocos.empty()) {
+        blocos.push(atual);
+        continue;
+    }
+    resultado = atual + blocos.top();
+    if (resultado == 111) {
+        blocos.pop();
+        pontos += 10;
+    } else {
+        blocos.push(atual);
+    }
+    if (blocos.size() >= M) {
+        cout << "game over";
+        return 0;
+    }
+}
+cout << pontos;
+
+}
